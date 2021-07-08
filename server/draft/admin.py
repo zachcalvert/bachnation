8897ad4e2ad4c038785384eb8a06c559e587f475
Zach Calvert <zcalvert@oreilly.com>
2021-07-07 20:18:43 -0700

    add more admin actions

--- server/draft/admin.py
+++ server/draft/admin.py
@@ -18,10 +18,37 @@ def mark_as_drafted(modeladmin, request, queryset):
 mark_as_drafted.short_description = 'Mark as drafted'
 
 
+def grant_rose(modeladmin, request, queryset):
+    for contestant in queryset:
+        contestant.roses += 1
+        contestant.save()
+mark_as_drafted.short_description = 'Grant Rose'
+
+def revoke_rose(modeladmin, request, queryset):
+    for contestant in queryset:
+        contestant.roses -= 1
+        contestant.save()
+mark_as_drafted.short_description = 'Revoke Rose'
+
+
+def eliminate(modeladmin, request, queryset):
+    for contestant in queryset:
+        contestant.eliminated = True
+        contestant.save()
+mark_as_drafted.short_description = 'Eliminate'
+
+
+def bring_back(modeladmin, request, queryset):
+    for contestant in queryset:
+        contestant.eliminated = False
+        contestant.save()
+mark_as_drafted.short_description = 'Bring Back'
+
+
 class ContestantAdmin(admin.ModelAdmin):
     list_display = ('name', 'profession', 'age',)
     search_fields = ['name', 'profession']
-    actions = [mark_as_undrafted, mark_as_drafted]
+    actions = [mark_as_undrafted, mark_as_drafted, grant_rose, revoke_rose, eliminate, bring_back]
 
 
 admin.site.register(Contestant, ContestantAdmin)
